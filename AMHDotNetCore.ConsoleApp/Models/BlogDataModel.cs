e8b865e19a5f3592cb31d5479b3c7699d5c71df9
aungminhein17 <aungminhein20202020@gmail.com>
2023-12-28 19:45:56 +0630

    EFCore EXAMPLES

--- AMHDotNetCore.ConsoleApp/Models/BlogDataModel.cs
+++ AMHDotNetCore.ConsoleApp/Models/BlogDataModel.cs
@@ -10,7 +10,6 @@ namespace AMHDotNetCore.ConsoleApp.Models
     {
         public int Blog_Id { get; set; }
         public string Blog_Title { get; set; }
-
         public string Blog_Content { get; set; }
 
         public string Blog_Author { get; set; }
